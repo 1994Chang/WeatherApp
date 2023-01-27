bc06ff43426ac2d416331be256fd324b6d475ef5
Onajite Kelvin <onajiteokeh@gmail.com>
2023-01-27 08:28:17 +0100

    axios

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,9 @@ export default function Home() {
   const [weather, setWeather] = useState({})
   const [loading, setLoading] = useState(false)
   const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
-
+  axios.get(url).then((response) => {
+    setWeather(response.data)
+  })
   const fetchWeather = (e) => {
     e.preventDefault()
     setLoading(true)
